6ec2bdbc2dcfec8c6e1c70ddd63f32377517136a
Steven Lu <tacticalazn@gmail.com>
2015-10-12 16:46:11 -0400

    actual deploys!

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 var _ = require('lodash');
 var dotenv = require('dotenv');
+var path = require('path')
 
 // run in silent mode
 dotenv.config({silent:true})
@@ -11,7 +12,9 @@ var config = {
   URL: "http://127.0.0.1",
   GITHUB_CLIENT_ID: "",
   GITHUB_CLIENT_SECRET: "",
-  SESSION_KEY: ""
+  SESSION_KEY: "",
+  SCRIPTS_DIR: path.join(__dirname, '../scripts/'),
+  BUILD_DIR: path.join(__dirname, '../.tmp/')
 };
 config = _.defaults(process.env, config);
 
